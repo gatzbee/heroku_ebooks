7c9cc7c08322600cd520aa58fd59ddea5370d110
gatzbee <greg@replaypoker.com>
2016-03-30 07:20:16 -0700

    Update local_settings_example.py

--- local_settings_example.py
+++ local_settings_example.py
@@ -3,15 +3,15 @@ Local Settings for a heroku_ebooks account. #fill in the name of the account you
 '''
 
 #configuration
-MY_CONSUMER_KEY = 'Your Twitter API Consumer Key'
-MY_CONSUMER_SECRET = 'Your Consumer Secret Key'
-MY_ACCESS_TOKEN_KEY = 'Your Twitter API Access Token Key'
-MY_ACCESS_TOKEN_SECRET = 'Your Access Token Secret'
+MY_CONSUMER_KEY = 'm5Yo5cA0Wz6jXFKe2aLEhTLiI'
+MY_CONSUMER_SECRET = 'badSbsc7lhAlp7diGEsX41XFWm8UXiVz7EsnidOUQbBLcrOk0k'
+MY_ACCESS_TOKEN_KEY = '67645636-O0faN878wJoWb4P1jqXtJRghHlHntDl1p5SLeugk6'
+MY_ACCESS_TOKEN_SECRET = '	ECIh0sYhvNc5KqZmhXCJFmfOhVJfTLGTVuqGpv0mU9CR4'
 
-SOURCE_ACCOUNTS = [""] #A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
+SOURCE_ACCOUNTS = ["@gatzby, @oddestbrain, @haishidao"] #A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
 ODDS = 8 #How often do you want this to run? 1/8 times?
 ORDER = 2 #how closely do you want this to hew to sensical? 1 is low and 3 is high.
 DEBUG = True #Set this to False to start Tweeting live
 STATIC_TEST = False #Set this to True if you want to test Markov generation from a static file instead of the API.
 TEST_SOURCE = ".txt" #The name of a text file of a string-ified list for testing. To avoid unnecessarily hitting Twitter API.
-TWEET_ACCOUNT = "" #The name of the account you're tweeting to.
+TWEET_ACCOUNT = "theoddestbrain" #The name of the account you're tweeting to.
